b3b7dcdb11ce0ef61a3ff64d5ebb7e9544ae77eb
Antarcticaa <32089950+Antarcticaa@users.noreply.github.com>
2017-10-31 16:21:49 +0800

    hahaha

---
+++ hhh.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	puts("please answer my questions!(¡ñ'-'¡ñ)!");
+	puts("question one:are you a gay??? press 1 if you are and press 0 if you are not.");  scanf("%d",&a);
+	switch(a){
+	case 1:puts("byebye /(¨Òo¨Ò)/~~"); break;
+	case 0:puts("question two:do you like me£¿(*£þ¨Œ£þ*)¥Ö?press 1 if you do and press 0 if you don't");  scanf("%d",&b);
+	       do{
+	       puts("question two:do you like me£¿(*£þ¨Œ£þ*)¥Ö?press 1 if you do and press 0 if you don't");  scanf("%d",&b);
+		   }while(b==0);
+	       puts("then be my boyfrined!!!(*^_^*).\nI love you !!!!!!!!!!!!");
+		   }
+		   return 0; 
+}
